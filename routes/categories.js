68d41c1eb1cd034b9c8cf2663c2a2f872d02dc33
muyi <eroosamuyi11451@gmail.com>
2022-05-15 15:34:08 +0100

    first commit

---
+++ routes/categories.js
@@ -0,0 +1,74 @@
+const {Category} = require('../models/category');
+const express = require('express');
+const router = express.Router();
+
+router.get(`/`, async (req, res) =>{
+    Category.find().then(categoryList => {
+        if(!categoryList) {
+            res.status(500).json({success: false})
+        } 
+        res.status(200).send(categoryList);
+
+    } )
+})
+
+router.get('/:id', async(req,res)=>{
+    const { id } = req.params
+    console.log(id)
+    if (id){
+    Category.findById(id).then(category => {
+        if(!category) {
+            res.status(500).json({message: 'The category with the given ID was not found.'})
+        } 
+        res.status(200).send(category);
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+  }
+})
+
+
+
+router.post('/', async (req,res)=>{
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    })
+    category = await category.save();
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
+    res.send(category);
+})
+
+
+router.put('/:id',async (req, res)=> {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon || category.icon,
+            color: req.body.color,
+        },
+        { new: true}
+    )
+
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
+    res.send(category);
+})
+
+router.delete('/:id', (req, res)=>{
+    Category.findByIdAndRemove(req.params.id)
+    .then(category =>{
+        if(category) {
+            return res.status(200).json({success: true, message: 'the category is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "category not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+module.exports = router;
